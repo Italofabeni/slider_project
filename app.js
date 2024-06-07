87aaac0f42487cea2c10c1be977a3c161ee801d9
italo <italofabeni234@gmail.com>
2024-06-07 11:08:25 -0300

    slider_gamer_project

---
+++ app.js
@@ -0,0 +1,45 @@
+let nextDom = document.getElementById('next');
+let prevDom = document.getElementById('prev');
+let carouselDom = document.querySelector('.carousel');
+let listItemDom = document.querySelector('.carousel .list');
+let thubmnailDom = document.querySelector('.carousel .thumbnail');
+
+nextDom.onclick = function(){
+    showSlider('next');
+}
+prevDom.onclick = function(){
+    showSlider('prev');
+}
+let timeRunning = 3000;
+let timeAutonext = 7000;
+let runTimeOut;
+let  runAutoRun = setTimeout(() => {
+    nextDom.click();
+}, timeAutonext);
+function showSlider(type){
+    let itemSlider = document.querySelectorAll('.carousel .list .item');
+    let itemThumbanil = document.querySelectorAll('.carousel .thumbnail .item')
+
+    if(type === 'next'){
+        listItemDom.appendChild(itemSlider[0]);
+        thubmnailDom.appendChild(itemThumbanil[0]);
+        carouselDom.classList.add('next')
+    }else{
+        let positionLastItem = itemSlider.length - 1;
+        listItemDom.prepend(itemSlider[positionLastItem]);
+        thubmnailDom.prepend(itemThumbanil[positionLastItem]);
+        carouselDom.classList.add('prev');
+    }
+
+    clearTimeout(runTimeOut);
+    runTimeOut = setTimeout(() => {
+        carouselDom.classList.remove('next');
+        carouselDom.classList.remove('prev');
+    }, timeRunning)
+
+    clearTimeout(runAutoRun);
+    runAutoRun = setTimeout(() => {
+        nextDom.click();
+    }, timeAutonext);
+
+}
